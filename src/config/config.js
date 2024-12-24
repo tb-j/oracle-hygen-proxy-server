1f62823ffed0d46e6297d0dc092bd8ea9b21e237
Tomasz Bierbasz-Juchnowicz <tomasz.bierbasz.juchnowicz@oracle.com>
2024-12-24 10:45:32 +0100

    .

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    port: process.env.PORT || 5005,
-    targetUrl: process.env.TARGET_URL || 'https://api.heygen.com/v1',
-    proxyPath: process.env.PROXY_PATH || '/api'
-};
